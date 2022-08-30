cf941fc1a6ceeea6f6f31840d892fe3449b2190c
Santiago Chaves <santiagochaves242@gmail.com>
2022-08-30 17:34:35 -0300

    Cambios Finales

--- homebanking/settings.py
+++ homebanking/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = 'homebanking.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'itbank3.db',
+        'NAME': BASE_DIR / 'itbank.db',
     }
 }
 
